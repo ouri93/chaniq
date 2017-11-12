19b334f47b6f7a3bffb361a1d9b123aa2623fb9a
ouri93 <sangchankim93@gmail.com>
2017-11-12 02:43:55 -0600

    Branch 0.0.5 - 11112017

---
+++ js/jquery.js
@@ -0,0 +1,35 @@
+/* 
+ * Javascript for moving items between two list boxes
+ */
+/*
+ * Original example found here: http://www.jquerybyexample.net/2012/05/how-to-move-items-between-listbox-using.html
+ * Modified by Esau Silva to support 'Move ALL items to left/right' and add better stylingon on Jan 28, 2016.
+ * 
+ */
+
+$(function () {
+    $('#btnRight').click(function (e) {
+        var selectedOpts = $('#lstBox1 option:selected');
+        if (selectedOpts.length == 0) {
+            alert("Nothing to move.");
+            e.preventDefault();
+        }
+
+        $('#lstBox2').append($(selectedOpts).clone());
+        $(selectedOpts).remove();
+        e.preventDefault();
+    });
+
+    $('#btnLeft').click(function (e) {
+        var selectedOpts = $('#lstBox2 option:selected');
+        if (selectedOpts.length == 0) {
+            alert("Nothing to move.");
+            e.preventDefault();
+        }
+
+        $('#lstBox1').append($(selectedOpts).clone());
+        $(selectedOpts).remove();
+        e.preventDefault();
+    });
+
+});
