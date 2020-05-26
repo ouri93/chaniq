396d930cf2ac93e1ade35a491e6d68355ed8c569
ouri93 <sangchankim93@gmail.com>
2020-05-25 19:21:00 -0500

    GUI Display Expansion - 0.0.7

--- content/new_vs.php
+++ content/new_vs.php
@@ -14,7 +14,8 @@ if ($_SESSION['role'] == 'guest'){
 		<!-- IFrame content here -->
 		<!-- Default Page content - HTTP Profile -->
 		<legend>Virtual Server Configuration</legend>
-		<iframe src="/content/if_new_vs.php" width="720px" height="600" frameborder="0"></iframe>       	
+		<!--  <iframe src="/content/if_new_vs.php" width="720px" height="600" frameborder="0"></iframe>  -->
+		<iframe src="/content/if_new_vs.php" width="1105px" height="92%" frameborder="0"></iframe>
         </fieldset>
     </form>
 </div>
